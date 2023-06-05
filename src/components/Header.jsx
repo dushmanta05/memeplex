8a7e6794bacadafa47450c89941077ba984be367
Dushmanta <dushmanta0511@gmail.com>
2023-06-05 23:45:42 +0530

    add project to github

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    
+    return (
+        <header className="header">
+            <img src="troll-face-png.png" className="header-img"/>
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header-text">Choose an image, write your text, make your meme 👻</h4>
+        </header>
+    )
+}
